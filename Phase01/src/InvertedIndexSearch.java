5669f0f9ff5e8f01e687587f90c8fc81c7b410a6
alirezamhm <alireza.am1379@gmail.com>
2021-02-22 20:36:49 +0330

    Added Query search

--- Phase01/src/InvertedIndexSearch.java
+++ Phase01/src/InvertedIndexSearch.java
@@ -8,9 +8,12 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InvertedIndexSearch {
-    public static HashMap<String, ArrayList<String>> indexMap = new HashMap<String, ArrayList<String>>();
+    public static HashMap<String, ArrayList<Entry>> indexMap = new HashMap<String, ArrayList<Entry>>();
+    public static HashSet<Entry> allDocs = new HashSet<Entry>();
 
     final static Set<String> stopWords = new HashSet<>(Arrays.asList("ourselves", "hers", "between", "yourself", "but",
             "again", "there", "about", "once", "during", "out", "very", "having", "with", "they", "own", "an", "be",
@@ -25,22 +28,132 @@ public class InvertedIndexSearch {
             "than"));
 
     public static void main(String[] args) {
-        ReadFiles();
+        readFiles();
 
-        // int i = 0;
-        // for (String word : indexMap.keySet()) {
-        //     System.out.println(word);
-        //     System.out.println(indexMap.get(word).toString());
-        //     if (++i > 5)
-        //         break;
-        // }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter search word or enter 'exit()' to end: ");
+        String inputStr = "";
+        while (true) {
+            inputStr = stemWord(scanner.nextLine().trim());
+            if (inputStr.equals("exit()"))
+                break;
+            if (inputStr.contains(" ") || inputStr.contains("+") || inputStr.contains("-")) {
+                printResult(searchQuery(inputStr), true);
+            } else {
+                printResult(searchWord(inputStr), false);
+            }
+
+        }
+
     }
 
-    public static void ReadFiles() {
+    public static void printResult(HashSet<Entry> searchResult, boolean isQuery) {
+        if (searchResult.isEmpty()) {
+            System.out.println("No Result found");
+        } else {
+            if (isQuery) {
+                // for(Entry entry: searchResult){
+                // System.out.println(entry.getDocName()+);
+                // }
+                searchResult.forEach((key) -> System.out.println(key.getDOCName()));
+
+            } else {
+                System.out.println(searchResult.toString());
+            }
+        }
+    }
+
+    public static HashSet<Entry> searchQuery(String query) {
+        HashSet<Entry> result = new HashSet<Entry>();
+        HashSet<Entry> normalSet = new HashSet<Entry>();
+        HashSet<Entry> plusSet = new HashSet<Entry>();
+        HashSet<Entry> minusSet = new HashSet<Entry>();
+
+        ArrayList<String> plus = new ArrayList<String>();
+        ArrayList<String> minus = new ArrayList<String>();
+        ArrayList<String> normal = new ArrayList<String>();
+
+        Pattern pattern = Pattern.compile("\\+(\\w+)");
+        Matcher matcher = pattern.matcher(query);
+        while (matcher.find()) {
+            plus.add(matcher.group(1));
+        }
+        pattern = Pattern.compile("-(\\w+)");
+        matcher = pattern.matcher(query);
+        while (matcher.find()) {
+            minus.add(matcher.group(1));
+        }
+        pattern = Pattern.compile("\\s(\\w+)");
+        matcher = pattern.matcher(" " + query);
+        while (matcher.find()) {
+            normal.add(matcher.group(1));
+        }
+
+        boolean flag = false;
+        for (String normalStr : normal) {
+            if (!flag) {
+                normalSet = searchWord(normalStr);
+                flag = true;
+            } else {
+                normalSet.retainAll(searchWord(normalStr));
+
+            }
+        }
+
+        for (String plusStr : plus) {
+            plusSet.addAll(searchWord(plusStr));
+        }
+        for (String minusStr : minus) {
+            minusSet.addAll(searchWord(minusStr));
+        }
+
+        // add normal set, Intersect plus set, Minus minus set'
+        if (normalSet.isEmpty()) {
+            result.addAll(plusSet);
+        } else {
+            result.addAll(normalSet);
+            if (!plusSet.isEmpty()) {
+                result.retainAll(plusSet);
+            }
+        }
+        if(result.isEmpty()){
+            result.addAll(allDocs);
+        }
+        result.removeAll(minusSet);
+
+        return result;
+    }
+
+    public static HashSet<Entry> searchWord(String word) {
+
+        HashSet<Entry> result = new HashSet<Entry>();
+        String word_s = "";
+        if (word.charAt(word.length() - 1) == 's') {
+            word_s = word.substring(0, word.length() - 1);
+        } else {
+            word_s = word + "s";
+        }
+        if (indexMap.containsKey(word)) {
+            result.addAll(indexMap.get(word));
+        }
+        if (indexMap.containsKey(word_s)) {
+            result.addAll(indexMap.get(word_s));
+        }
+        return result;
+    }
+
+    public static String stemWord(String word) {
+        String stemmed = word.toLowerCase().replace("'s", "");
+        stemmed = stemmed.replace(",", "");
+        return stemmed;
+    }
+
+    public static void readFiles() {
         File directory = new File("Phase01/data/EnglishData");
         File fileList[] = directory.listFiles();
         for (File file : fileList) {
             String id = file.getName();
+            allDocs.add(new Entry(id , 0));
             try {
                 Scanner scanner = new Scanner(file);
                 while (scanner.hasNextLine()) {
@@ -56,20 +169,58 @@ public class InvertedIndexSearch {
 
     public static void addWords(String id, String content) {
         String[] listOfWords = getListOfWords(content);
-        for (String word : listOfWords) {
-            word = word.toLowerCase();
+        for (int i = 0; i < listOfWords.length; i++) {
+            String word = stemWord(listOfWords[i]);
             if (stopWords.contains(word))
                 continue;
             if (indexMap.containsKey(word)) {
-                indexMap.get(word).add(id);
+                indexMap.get(word).add(new Entry(id, i + 1));
             } else {
-                indexMap.put(word, new ArrayList<String>(Arrays.asList(id)));
+                indexMap.put(word, new ArrayList<Entry>(Arrays.asList(new Entry(id, i + 1))));
             }
         }
     }
 
     public static String[] getListOfWords(String content) {
         return content.split("\\s+");
+    }
+
+}
+
+class Entry {
+    private String DOCName;
+    private int index;
+
+    Entry(String DOCName, int index) {
+        this.DOCName = DOCName;
+        this.index = index;
+    }
+
+    String getDOCName() {
+        return this.DOCName;
+    }
+
+    int getIndex() {
+        return this.index;
+    }
+
+    @Override
+    public String toString() {
+        return "Document Id: " + this.DOCName + ", Index: " + this.index;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Entry)) {
+            return false;
+        }
+        Entry e = (Entry) o;
+        return this.DOCName.equals(e.DOCName);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.DOCName.hashCode();
     }
 
 }
