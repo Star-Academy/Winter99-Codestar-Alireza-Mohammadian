0379eae5bf06a738f6e266c88a6eb68447abaf5c
alirezamhm <alireza.am1379@gmail.com>
2021-02-24 20:52:33 +0330

    refactored search method and stop words

--- Phase01/src/InvertedIndexSearch.java
+++ Phase01/src/InvertedIndexSearch.java
@@ -14,24 +14,6 @@ import java.util.regex.Pattern;
 
 public class InvertedIndexSearch {
 
-    static class StopWordsClass {
-        private static final Set<String> STOP_WORDS = new HashSet<>(Arrays.asList("ourselves", "hers", "between",
-                "yourself", "but", "again", "there", "about", "once", "during", "out", "very", "having", "with", "they",
-                "own", "an", "be", "some", "for", "do", "its", "yours", "such", "into", "of", "most", "itself", "other",
-                "off", "is", "s", "am", "or", "who", "as", "from", "him", "each", "the", "themselves", "until", "below",
-                "are", "we", "these", "your", "his", "through", "don", "nor", "me", "were", "her", "more", "himself",
-                "this", "down", "should", "our", "their", "while", "above", "both", "up", "to", "ours", "had", "she",
-                "all", "no", "when", "at", "any", "before", "them", "same", "and", "been", "have", "in", "will", "on",
-                "does", "yourselves", "then", "that", "because", "what", "over", "why", "so", "can", "did", "not",
-                "now", "under", "he", "you", "herself", "has", "just", "where", "too", "only", "myself", "which",
-                "those", "i", "after", "few", "whom", "t", "being", "if", "theirs", "my", "against", "a", "by", "doing",
-                "it", "how", "further", "was", "here", "than"));
-
-        public static Set<String> getStopWords() {
-            return STOP_WORDS;
-        }
-    }
-
     private HashMap<String, ArrayList<Entry>> indexMap = new HashMap<String, ArrayList<Entry>>();
     private HashSet<Entry> allDocs = new HashSet<Entry>();
     private static final String NORMAL_PATTERN = "\\s(\\w+)";
@@ -48,11 +30,8 @@ public class InvertedIndexSearch {
     }
 
     public Result search(String input) {
-        if (input.contains(" ") || input.contains("+") || input.contains("-")) {
-            return new Result(this.searchQuery(input), true);
-        } else {
-            return new Result(this.searchWord(input), false);
-        }
+        Boolean multiWords = input.contains(" ") || input.contains("+") || input.contains("-");
+        return new Result(this.searchQuery(input), multiWords);
     }
 
     private HashSet<Entry> searchQuery(String query) {
@@ -108,7 +87,7 @@ public class InvertedIndexSearch {
         String[] listOfWords = getListOfWords(content);
         for (int i = 0; i < listOfWords.length; i++) {
             String word = listOfWords[i];
-            if (StopWordsClass.getStopWords().contains(word))
+            if (StopWordsClass.STOP_WORDS.contains(word))
                 continue;
             if (indexMap.containsKey(word)) {
                 indexMap.get(word).add(new Entry(id, i + 1));
