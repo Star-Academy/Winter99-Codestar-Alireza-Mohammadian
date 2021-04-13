d0fdac52c6d378d67b8071842a2baf558bfca430
alirezamhm <alireza.am1379@gmail.com>
2021-04-13 20:48:16 +0430

    one test fixed

--- Phase10/SearchApp/SearchTest/ElasticTest.cs
+++ Phase10/SearchApp/SearchTest/ElasticTest.cs
@@ -12,6 +12,7 @@ namespace SearchTest
         private static string indexName = "test-index";
         private Elastic elastic = new Elastic(indexName, new Uri("http://localhost:9200"));
 
+        [Fact]
         public void CreateClientTest()
         {
             var pingResponse = elastic.Client.Ping();
