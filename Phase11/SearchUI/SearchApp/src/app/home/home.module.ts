f4b212188453b9f4eac2c7b020d54d66451948ff
alirezamhm <alireza.am1379@gmail.com>
2021-05-12 13:45:38 +0430

    home title and search bar added

--- Phase11/SearchUI/SearchApp/src/app/home/home.module.ts
+++ Phase11/SearchUI/SearchApp/src/app/home/home.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { TitleComponent } from './title/title.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    TitleComponent,
+    SearchbarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule
   ],
   exports: [
     HomeComponent
